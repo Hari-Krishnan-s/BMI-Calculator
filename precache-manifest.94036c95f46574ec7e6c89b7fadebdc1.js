def97be2f0d4c4092ebe517578a59c2937c626c5
Harikrishnan s <harikrishnansajikumar123@gmail.com>
2021-06-20 21:54:57 +0530

    Updates

---
+++ precache-manifest.94036c95f46574ec7e6c89b7fadebdc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3be3b9574c9aa8b7467a654560c8c2",
+    "url": "/BMI-Calculator/index.html"
+  },
+  {
+    "revision": "d5599e2ead90020f0923",
+    "url": "/BMI-Calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "283d34c126dd935daf99",
+    "url": "/BMI-Calculator/static/css/main.160e4576.chunk.css"
+  },
+  {
+    "revision": "d5599e2ead90020f0923",
+    "url": "/BMI-Calculator/static/js/2.9480e6f5.chunk.js"
+  },
+  {
+    "revision": "283d34c126dd935daf99",
+    "url": "/BMI-Calculator/static/js/main.39a7e2a6.chunk.js"
+  },
+  {
+    "revision": "0fef11334b7193e979e2",
+    "url": "/BMI-Calculator/static/js/runtime~main.34b18d77.js"
+  }
+]);
